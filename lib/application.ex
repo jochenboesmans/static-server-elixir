c4e5a714096e3dccea8bf6c871bd212b27f24971
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-07-01 14:37:30 +0200

    Mix format.

--- lib/application.ex
+++ lib/application.ex
@@ -3,16 +3,19 @@ defmodule StaticServerElixir.Application do
   use Application
 
   def start(_type, _args) do
-		# Customize port in config.exs.
-		cfg_port = Application.get_env(:static_server_elixir, :port)
+    # Customize port in config.exs.
+    cfg_port = Application.get_env(:static_server_elixir, :port)
+
     children = [
       {Plug.Cowboy, scheme: :http, plug: StaticServerElixir.Router, options: [port: cfg_port]}
     ]
+
     options = [
       # In case of multiple children, all will be restarted when one crashes.
       strategy: :one_for_all,
       name: __MODULE__
     ]
+
     Supervisor.start_link(children, options)
   end
 end
